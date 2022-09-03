d94d4ccf240bacf2fe62aa8b5f215da5866c0645
Martin <herrlein.martin@gmail.com>
2022-09-02 22:17:08 -0300

    se arregla enviroment y se agrega icono de la web

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,7 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: true,
+  production: false,
   //urlApi: 'http://localhost:8080'
   urlApi: 'http://ec2-3-83-173-159.compute-1.amazonaws.com'
 };
