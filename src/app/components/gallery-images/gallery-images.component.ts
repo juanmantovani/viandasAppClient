92b4910bb18c2353be0666da5f430b62192a3149
Martin <herrlein.martin@gmail.com>
2022-11-11 12:18:57 -0300

    se agrega gallery de imagenes en la categoria de la pag de inicio

---
+++ src/app/components/gallery-images/gallery-images.component.ts
@@ -0,0 +1,38 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { GalleryItem, ImageItem, Gallery, GalleryRef } from 'ng-gallery';
+import { environment } from 'src/environments/environment';
+
+@Component({
+  selector: 'app-gallery-images',
+  templateUrl: './gallery-images.component.html',
+  styleUrls: ['./gallery-images.component.css'],
+})
+export class GalleryImagesComponent implements OnInit {
+  @Input() imagesCategory : any []; 
+  images: GalleryItem[] = [];
+  URLAPI = environment.urlApi;
+  
+  galleryId = 'foodGallery';
+
+
+  constructor(private gallery: Gallery) { 
+
+  }
+
+
+  ngOnInit() {
+
+    const galleryRef: GalleryRef = this.gallery.ref(this.galleryId);
+
+    this.imagesCategory?.forEach(image => {
+      //this.images.push(new ImageItem({src: this.URLAPI+image.urlImage, thumb: this.URLAPI+image.urlImage}));
+      galleryRef.addImage({
+        src: this.URLAPI+image.urlImage,
+        thumb: this.URLAPI+image.urlImage,
+        title: image.title
+      });
+    })
+
+  }
+
+}
