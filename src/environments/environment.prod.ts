1c07b537cf793224470aaeec33566c2bcbcad06e
Juan Cruz Mantovani <43190115+juanmantovani@users.noreply.github.com>
2023-05-23 18:53:37 +0200

    Update environment.prod.ts

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   urlApi: 'https://backend.integralviandas.com.ar/app',
-  urlDiscordLogg: 'https://discord.com/api/webhooks/1108498402959958086/jZpcS6iBLIHxAaspmSehwwW9zAJ7vCw2thiqicWRVHhtNilGewJKQksC_rr1GXPd0ns4',
+  urlDiscordLogg: '',
   production: true
 };
